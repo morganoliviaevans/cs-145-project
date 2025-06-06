9e1537152717e57b5b322fe09bc0a2f5c5cdc06e
morganoliviaevans <morganoliviaevans@gmail.com>
2025-06-06 12:13:46 -0700

    Implemented separate LEDs and adjusted logic accordingly
    - Removed LED strip, sad sad
    - Commented out LED strip logic to use at a later date
    - We are defeated but we will prevail

--- proj-5/src/helper.c
+++ proj-5/src/helper.c
@@ -9,92 +9,113 @@
 // HELPER FUNCTIONS
 ///////////////////
 
-////////////////////////////////////////// LED STRIP //////////////////////////////////////////
+////////////////////////////////////////// LEDS //////////////////////////////////////////
 
-struct cRGB leds[8]; // Pre-built struct from light_ws2812 library with internal uint8_t r, g, b
+int GREEN_LED = 0;
+int YELLOW_LED = 1;
+int RED_LED = 2;
+int FLASH1_LED = 3;
+int FLASH2_LED = 5;
+int FLASH3_LED = 6;
+// struct cRGB leds[8]; // Pre-built struct from light_ws2812 library with internal uint8_t r, g, b
 
 void led_init() {
-    DDRD |= (1 << PD5);  // Make PD5 output
-    turn_off_leds();
-    avr_wait(2000);
+    // Set GREEN, YELLOW, RED LED pins to output
+    DDRB |= (1 << PIN0) | (1 << PIN1) | (1 << PIN2);
+    // Set FLASH1, FLASH2, FLASH3 LED pins to output
+    DDRD |= (1 << PIN3) | (1 << PIN5) | (1 << PIN6);
+    // DDRD |= (1 << PD5);  // Make PD5 output
 }
 
 void green_led() {
-    // Ensure all LEDs are off before turning on green LEDs
-    turn_off_leds();
-    // Set LEDs 0-7 to green
-    for (int i = 0; i < 8; ++i) {
-        leds[i].r = 255;
-        leds[i].g = 0;
-        leds[i].b = 0;
-    }
-    ws2812_setleds(leds, 8); // Send to LED strip
+    // Ensure red and yellow LEDs are off before turning on green LED
+    CLR_BIT(PORTD, RED_LED);    // Turn off the red LEDMore actions
+    CLR_BIT(PORTD, YELLOW_LED); // Turn off the yellow LED
+    SET_BIT(PORTB, GREEN_LED);  // Turn on the green LED
+
+    // // Set LEDs 0-7 to green
+    // for (int i = 0; i < 8; ++i) {
+    //     leds[i].r = 255;
+    //     leds[i].g = 0;
+    //     leds[i].b = 0;
+    // }
+    // ws2812_setleds(leds, 8); // Send to LED strip
 }
 
 void yellow_led() {
-    // Ensure all LEDs are off before turning on yellow LEDs
-    turn_off_leds();
-    // Set LEDs 0-3 to green
-    for (int i = 0; i < 4; ++i) {
-        leds[i].r = 0;
-        leds[i].g = 255;
-        leds[i].b = 0;
-    }
-    // Set LEDs 4-7 to yellow
-    for (int i = 4; i < 8; ++i) {
-        leds[i].r = 255;
-        leds[i].g = 255;
-        leds[i].b = 0;
-    }
-    ws2812_setleds(leds, 8); // Send to LED strip
+    // Ensure red LED is off before turning on yellow LED
+    CLR_BIT(PORTD, RED_LED);    // Turn off the red LEDMore actions
+    SET_BIT(PORTB, GREEN_LED);  // Turn on the green LED
+    SET_BIT(PORTB, YELLOW_LED); // Turn on the yellow LED
+    // // Set LEDs 0-3 to green
+    // for (int i = 0; i < 4; ++i) {
+    //     leds[i].r = 0;
+    //     leds[i].g = 255;
+    //     leds[i].b = 0;
+    // }
+    // // Set LEDs 4-7 to yellow
+    // for (int i = 4; i < 8; ++i) {
+    //     leds[i].r = 255;
+    //     leds[i].g = 255;
+    //     leds[i].b = 0;
+    // }
+    // ws2812_setleds(leds, 8); // Send to LED strip
 }
 
 void red_led() {
-    // Ensure all LEDs are off before turning on red LEDs
-    turn_off_leds();
-    // Set all LEDs to red
-    for (int i = 0; i < 8; ++i) {
-        leds[i].r = 255;
-        leds[i].g = 0;
-        leds[i].b = 0;
-    }
-    ws2812_setleds(leds, 8); // Send to LED strip
+    SET_BIT(PORTB, GREEN_LED);  // Turn on the green LED
+    SET_BIT(PORTB, YELLOW_LED); // Turn on the yellow LED
+    SET_BIT(PORTB, RED_LED);    // Turn on the red LED
+    // // Set all LEDs to red
+    // for (int i = 0; i < 8; ++i) {
+    //     leds[i].r = 255;
+    //     leds[i].g = 0;
+    //     leds[i].b = 0;
+    // }
+    // ws2812_setleds(leds, 8); // Send to LED strip
 }
 
 void flash_led() {
     // Ensure all LEDs are off before flashing
     turn_off_leds();
+    // Turn on entire RGB LED for full white
+    SET_BIT(PORTD, FLASH1_LED); // Turn on the red LED
+    SET_BIT(PORTD, FLASH2_LED); // Turn on the blue LED
+    SET_BIT(PORTD, FLASH3_LED); // Turn on the green LED
+    avr_wait(500);
+
     // Flash all LEDs
-    for (int i = 0; i < 8; ++i) {
-        leds[i].r = 255;
-        leds[i].g = 255;
-        leds[i].b = 255;
-    }
-    ws2812_setleds(leds, 8); // Send to strip
-    avr_wait(500);           // Wait for 500 ms
-    turn_off_leds();         // Turn off LED strip
-    avr_wait(500);           // Wait for 500 ms
+    // for (int i = 0; i < 8; ++i) {
+    //     leds[i].r = 255;
+    //     leds[i].g = 255;
+    //     leds[i].b = 255;
+    // }
+    // ws2812_setleds(leds, 8); // Send to strip
 }
 
 void turn_off_leds() {
-    for (int i = 0; i < 8; ++i) {
-        leds[i].r = 0;
-        leds[i].g = 0;
-        leds[i].b = 0;
-    }
-    ws2812_setleds(leds, 8); // Send to LED strip
+    CLR_BIT(PORTD, RED_LED);    // Turn off the red LEDMore actions
+    CLR_BIT(PORTD, YELLOW_LED); // Turn off the yellow LED
+    CLR_BIT(PORTD, GREEN_LED);  // Turn off the green LED
+
+    // for (int i = 0; i < 8; ++i) {
+    //     leds[i].r = 0;
+    //     leds[i].g = 0;
+    //     leds[i].b = 0;
+    // }
+    // ws2812_setleds(leds, 8); // Send to LED strip
 }
 
 void led_test() {
-    turn_off_leds();
-    avr_wait(2000);
+    // turn_off_leds();
+    // avr_wait(2000);
     // Set all leds to red
-    for (int i = 0; i < 8; ++i) {
-        leds[i].r = 255;
-        leds[i].g = 0;
-        leds[i].b = 0;
-    }
-    ws2812_setleds(leds, 8); // Send to LED strip
+    // for (int i = 0; i < 8; ++i) {
+    //     leds[i].r = 255;
+    //     leds[i].g = 0;
+    //     leds[i].b = 0;
+    // }
+    // ws2812_setleds(leds, 8); // Send to LED strip
 }
 
 ////////////////////////////////////////// SOUND DETECTOR //////////////////////////////////////////
--- proj-5/src/main.c
+++ proj-5/src/main.c
@@ -25,56 +25,52 @@ int main() {
     typedef enum {LISTENING, FLASH} AudioState;
     AudioState audio_state = LISTENING;
     float audio_level = 0;
+    int red_counter = 0;
+    int red_max = 3;
     // Set PA1 as input
     AUDIO_DDR &= ~(1 << AUDIO_PIN);
     adc_init();
-    avr_wait(1000);
+    avr_wait(500);
 
     while(1) {
-        led_test();
-        avr_wait(1000);
-        turn_off_leds();
-        avr_wait(1000);
+        // LISTENING State
+        if (audio_state == LISTENING) {
+            // Get audio level from ADC
+            audio_level = get_audio_level();
+            // If sound level is at acceptable volume, LED is green
+            if (audio_level <= 20) {
+                // Set LED to green
+                green_led();
+            }
+
+            // If sound level is at elevated volume, LED is yellow
+            if (audio_level > 20 && audio_level <= 40) {
+                // Set LED to yellow
+                yellow_led();
+            }
+
+            // If sound level is at excessive volume, LED is red
+            if (audio_level > 40) {
+                // Set LED to red
+                red_led();
+                red_counter++;
+                // If it has been 3 infractions at excessive volume, enter FLASH state
+                if (red_counter > red_max) {
+                    audio_state = FLASH;
+                }
+            }
+        } 
+        // FLASH State
+        if (audio_state == FLASH) {
+            // Reset our counter while in FLASH state
+            red_counter = 0;
+            // Commence FLASH sequence for 30 seconds
+            int i = 0;
+            while (i < 60) {
+                flash_led();
+            }
+            // Go back to LISTEN state
+            audio_state = LISTENING;
+        } 
     }
-
-    // while(1) {
-    //     // LISTENING State
-    //     if (audio_state == LISTENING) {
-    //         // Get audio level from ADC
-    //         audio_level = get_audio_level();
-    //         // If sound level is at acceptable volume, LED is green
-    //         if (audio_level <= 20) {
-    //             // Set LED to green
-    //             green_led();
-    //         }
-
-    //         // If sound level is at elevated volume, LED is yellow
-    //         if (audio_level > 20 && audio_level <= 40) {
-    //             // Set LED to yellow
-    //             yellow_led();
-    //         }
-
-    //         // If sound level is at excessive volume, LED is red
-    //         if (audio_level > 40) {
-    //             // Set LED to red
-    //             red_led();
-    //             // Begin timer
-
-    //             // If it has been 5 seconds at excessive volume, enter FLASH state
-    //             // if (audio_level == 50) {
-    //             //     audio_state = FLASH;
-    //             // }
-    //         }
-    //     } 
-    //     // // FLASH State
-    //     // if (audio_state == FLASH) {
-    //     //     // Commence FLASH sequence for 30 seconds
-    //     //     int i = 0;
-    //     //     while (i < 60) {
-    //     //         flash_led();
-    //     //     }
-    //     //     // Go back to LISTEN state
-    //     //     audio_state = LISTENING;
-    //     // } 
-    // }
 }
